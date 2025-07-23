1d7aab839d70e5f659219153e451d5d4aba27539
MarvinKristMarvin <166423247+MarvinKristMarvin@users.noreply.github.com>
2025-07-23 10:45:23 +0200

    implemented full security (SQLI, XSS, CSRF, CSP, nosniff, input validation, secured auth, rate limiting)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,40 @@
 import type { NextConfig } from "next";
 
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  font-src 'self' https://fonts.gstatic.com;
+  img-src 'self' blob: data:;
+  media-src 'self' blob: data:;
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  upgrade-insecure-requests;
+`;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply to all routes
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader
+              .replace(/\n/g, "")
+              .replace(/\s{2,}/g, " ")
+              .trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
