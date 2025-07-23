fb7992a45a980142d097d99058c2a8439f89acbd
MarvinKristMarvin <166423247+MarvinKristMarvin@users.noreply.github.com>
2025-07-23 16:53:37 +0200

    normally sentry works properly with content policy now

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://294b1ae1d72aa4a7cdfb737af6a55d15@o4509717992636416.ingest.de.sentry.io/4509718005219408",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
